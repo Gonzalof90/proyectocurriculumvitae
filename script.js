9143220e22ee49c874dfa51b989ba884bd657f6e
Gonzalo Flores <floresgonzalomartin@gmail.com>
2023-01-31 17:24:46 -0300

    asd

--- script.js
+++ script.js
@@ -2,11 +2,40 @@
 
 let boton1 = document.getElementById("botonSobreMi")
 let cuadroSobreMi = document.getElementById("cuadroSobreMi")
-boton1.addEventListener("click",() =>{
-    cuadroSobreMi.classList.toggle("seleccionado")
+boton1.addEventListener("mousemove",() =>{
+    cuadroSobreMi.style.backgroundColor = "lightblue";
     })
-    let boton2 = document.getElementById("botonContacto")
-let cuadroContacto = document.getElementById("cuadroContacto")
-boton2.addEventListener("click",() =>{
-    cuadroContacto.classList.toggle("seleccionado")
+boton1.addEventListener("mouseout",() =>{
+    cuadroSobreMi.style.backgroundColor = "";
     })
+    boton1.addEventListener("mousemove",() =>{
+        cuadroSobreMi.style.display = "block"})
+  
+        boton1.addEventListener("mouseout",() =>{
+            cuadroSobreMi.style.display = "none"})
+      
+
+        // // boton1.removeEventListener("mouseout",
+        // //             cuadroSobreMi.style.display = "block",true
+        // //             )
+   
+        //     boton1.addEventListener("mouseout",() =>{
+        //         boton1.removeEventListener("mousemove",
+        //             cuadroSobreMi.style.display = "block",false
+        //             )
+        //         })
+          
+            
+
+        
+
+
+
+
+
+
+//     let boton2 = document.getElementById("botonContacto")
+// let cuadroContacto = document.getElementById("cuadroContacto")
+// boton2.addEventListener("click",() =>{
+//     cuadroContacto.classList.toggle("seleccionado")
+//     })
